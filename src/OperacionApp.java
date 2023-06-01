2065d8a7573e026bc56af604e006ad0db54444b8
Maikel <palango4887@gmail.com>
2023-06-01 09:49:29 +0200

    creacion de la operacion contarEdades

--- src/OperacionApp.java
+++ src/OperacionApp.java
@@ -1,4 +1,30 @@
 
 public class OperacionApp {
+	private int [] array;
 
+	public OperacionApp(int[] array) {
+		
+		this.array = array;
+	}
+
+	public int[] getArray() {
+		return array;
+	}
+
+	public void setArray(int[] array) {
+		this.array = array;
+	}
+	public void contarEdades(int [] array) {
+		int c=0,c1=0,c2=0;
+		for(int i=0; i<array.length;i++) {
+			if(array[i] >=65) {
+				c ++;
+			}else if(array[i]<18) {
+				c1++;
+			}else if(array[i]>=18) {
+				c2++;
+			}
+		}
+		System.out.println("Menores de edad " + c1 + " mayores de edad: " + c2 + " mayores de 65: " + c);
+	}
 }
--- src/PrincipalApp.java
+++ src/PrincipalApp.java
@@ -3,6 +3,10 @@ public class PrincipalApp {
 
 	public static void main(String[] args) {
 		
+		int [] array = {12,18,5,66,29,8,20,65};
+		OperacionApp operacion = new OperacionApp(array);
+		
+		operacion.contarEdades(array);
 		
 	}
 
